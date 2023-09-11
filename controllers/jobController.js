e89e32b4fadc861e51eb0e74604ba4922a8dda9b
Emilio Luis Tarnate <emilioluistarnate1@gmail.com>
2023-09-12 06:56:04 +0800

    added some changes and files

---
+++ controllers/jobController.js
@@ -0,0 +1,42 @@
+import Job from '../models/JobModel.js';
+
+export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
+  res.status(200).json({ jobs });
+};
+
+export const createJob = async (req, res) => {
+  const job = await Job.create(req.body);
+  res.status(201).json({ job });
+};
+
+export const getJob = async (req, res) => {
+  const { id } = req.params;
+
+  const job = await Job.findById(id);
+  if (!job) {
+    res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
+};
+
+export const updateJob = async (req, res) => {
+  const { id } = req.params;
+
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!updatedJob) {
+    res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ msg: 'job modified', job: updatedJob });
+};
+
+export const deleteJob = async (req, res) => {
+  const { id } = req.params;
+  const removedJob = await Job.findByIdAndDelete(id);
+  if (!removedJob) {
+    res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ msg: 'job deleted', job: removedJob });
+};
--- server.js
+++ server.js
@@ -1,22 +1,13 @@
+import 'express-async-errors';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
-import { nanoid } from 'nanoid';
+import mongoose from 'mongoose';
 
-let jobs = [
-  {
-    id: nanoid(),
-    company: 'apple',
-    position: 'front-end',
-  },
-  {
-    id: nanoid(),
-    company: 'google',
-    position: 'back-end',
-  },
-];
+// routers
+import jobRouter from './routes/jobRouter.js';
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -33,65 +24,7 @@ app.post('/', (req, res) => {
   res.json({ message: 'data received', data: req.body });
 });
 
-// GET ALL JOBS
-app.get('/api/v1/jobs', (req, res) => {
-  res.status(200).json({ jobs });
-});
-
-// CREATE JOB
-app.post('/api/v1/jobs', (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
-
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(201).json({ job });
-});
-
-// GET SINGLE JOB
-app.get('/api/v1/jobs/:id', (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  res.status(200).json({ job });
-});
-
-// EDIT JOB
-app.patch('/api/v1/jobs/:id', (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: 'job modified', job });
-});
-
-// DELETE JOB
-app.delete('/api/v1/jobs/:id', (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-  res.status(200).json({ msg: 'job deleted' });
-});
-
-app.use('*', (req, res) => {
-  res.status(404).json({ msg: 'not found' });
-});
+app.use('/api/v1/jobs', jobRouter);
 
 app.use((err, req, res, next) => {
   console.log(err);
@@ -100,6 +33,12 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5100;
 
-app.listen(port, () => {
-  console.log(`server running on ${port}...`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on ${port}...`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
